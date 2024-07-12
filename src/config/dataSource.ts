68e7eb22721b89dacc0fb1cd9d10214910856a6e
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-07-12 14:05:11 -0300

    Updated
    Refactoring to improve connection security with the DB.

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -2,14 +2,26 @@ import { DataSource } from "typeorm"
 import PetEntity from "../entities/PetEntity.js"
 import AdopterEntity from "../entities/AdopterEntity.js"
 import AddressEntity from "../entities/AddressEntity.js"
+import "dotenv/config"
+
+
+function parsePort(port: string | undefined): number | undefined {
+    if (port) {
+        const parsedPort = parseInt(port, 10);
+        if (!isNaN(parsedPort)) {
+            return parsedPort;
+        }
+    }
+    return undefined
+}
 
 export const AppDataSource = new DataSource({
-    type:"mysql",
-    host: "localhost",
-    port: 3306,
-    username: "adopet",
-    password: "admin",
-    database: "db_adopet",
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: parsePort(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [
         PetEntity,
         AdopterEntity,
