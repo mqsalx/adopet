5b2aae4cb89239d2b721e43d5167599497cdc31d
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-07-12 12:48:56 -0300

    Updated
    Refactoring...

--- src/entities/AdopterEntity.ts
+++ src/entities/AdopterEntity.ts
@@ -1,35 +1,40 @@
 import {
     Column,
     Entity,
+    JoinColumn,
+    OneToOne,
     PrimaryGeneratedColumn,
 } from "typeorm";
+import AddressEntity from "./AddressEntity.js";
 
-    @Entity()
-    export default class AdopterEntity {
-        @PrimaryGeneratedColumn()
-            id!: number;
-            @Column()
-            name: string;
-            @Column()
-            password: string;
-            @Column()
-            phone: string;
-            @Column({ nullable: true })
-            img_profile?: string;
-            @Column({ nullable: true })
-            address?: string;
+@Entity()
+export default class AdopterEntity {
+    @PrimaryGeneratedColumn()
+        id!: number
+        @Column()
+        name: string
+        @Column()
+        password: string
+        @Column()
+        phone: string
+        @Column({ nullable: true })
+        img_profile?: string
 
-        constructor(
-            name: string,
-            password: string,
-            phone: string,
-            img_profile?: string,
-            address?: string
-        ) {
-            this.name = name;
-            this.password = password;
-            this.phone = phone;
-            this.img_profile = img_profile;
-            this.address = address;
-        }
+        @OneToOne(() => AddressEntity, {nullable: true, cascade: true,eager: true})
+        @JoinColumn()
+        address?: AddressEntity
+
+    constructor(
+        name: string,
+        password: string,
+        phone: string,
+        img_profile?: string,
+        address?: AddressEntity
+    ) {
+        this.name = name
+        this.password = password
+        this.phone = phone
+        this.img_profile = img_profile
+        this.address = address
     }
+}
