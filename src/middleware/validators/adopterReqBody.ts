98941f6cf1e0e79b8386da0df7857adb33575128
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-07-15 16:21:55 -0300

    feat
    Added validations on password entry with yup and Regex.

--- src/middleware/validators/adopterReqBody.ts
+++ src/middleware/validators/adopterReqBody.ts
@@ -5,7 +5,7 @@ import { NextFunction, Request, Response } from "express"
 
 const schemaBodyAdopter: yup.Schema<Omit<TypeReqBodyAdopter, "address">> = yup.object({
     name: yup.string().defined().required(),
-    password: yup.string().defined().required().min(6),
+    password: yup.string().defined().required().min(6).matches(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/gm, "Invalid password!"),
     phone: yup.string().defined().required().matches(/^(\(?[0-9]{2}\)?)? ?([0-9]{4,5})-?([0-9]{4})$/gm, "Phone number is not valid!"),
     img_profile: yup.string().optional()
 })
